b793568c3881b6b87aa1dd9553170620c3e92b7e
Anderson Araujo <asain85@hotmail.com>
2022-08-18 13:51:02 -0300

    ADD: index.css com possiveis cores padrão

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom'
 import styles from './Navbar.module.css'
-
 import logo from '../../img/logo-mvp.png'
 import { AiOutlineMenu } from "react-icons/ai"
 
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,16 @@
+function Home(){
+    return (
+        <div className="home">
+            <br></br>
+            <h1>MVP Sports</h1>
+            <div className="promo">promo</div>
+            <div className="novidades">Novidades</div>
+            <div className="escolha">escolha sua nike</div>
+            <div className="novoEsporte">Experimente um novo esporte</div>
+            <div className="blog">BLOG</div>
+
+        </div>
+    )
+}
+
+export default Home
